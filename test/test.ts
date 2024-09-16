422a79efaf83eb125406b86f23afe208e115d666
Anthony W. Weed <devnoot@gmail.com>
2024-09-16 15:16:03 -0400

    Restructure

---
+++ test/test.ts
@@ -0,0 +1,8 @@
+import { parse_fon2 } from '../lib/utils'
+import fs from 'fs'
+import path from 'path'
+
+const fontToLoad = path.resolve('..', 'fonts', 'DBIGFONT-freedoom.fon2')
+
+console.info(fontToLoad)
+
