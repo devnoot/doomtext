c619100c0e4dee8ad70cf74bb5840428c96d0e5b
Anthony W. Weed <devnoot@gmail.com>
2024-09-16 04:54:57 -0400

    Updated DoomText.ts

--- lib/DoomText.ts
+++ lib/DoomText.ts
@@ -1,5 +1,20 @@
 export class DoomText {
 
+  private _text: string;
+
+  constructor() {
+    this._text = 'Rip and Tear. Until it is done!'
+  }
+
+
+
+  get text(): string {
+    return this._text
+  }
+
+  set text(value) {
+    this._text = value
+  }
   
 
 }
